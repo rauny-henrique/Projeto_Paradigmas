b7189daea01674e25ce71b7ab5570e97fe75cd96
RaunyHenrique <rauny22@gmail.com>
2016-10-14 16:21:41 -0300

    Update: 14/10

--- _javascript/_slickquizjs/master.js
+++ _javascript/_slickquizjs/master.js
@@ -3,4 +3,6 @@
 $(function () {
     $('#slickQuiz').slickQuiz();
     $('#slickQuiz2').slickQuiz({json: quizJSON2});// passar a JSON aqui.... mas declarar ela la em Conf..  $('#slickQuiz').slickQuiz({json: {YOUR_JSON_HERE}});
+    $('#slickQuiz3').slickQuiz({json: quizJSON3});// passar a JSON aqui.... mas declarar ela la em Conf..  $('#slickQuiz').slickQuiz({json: {YOUR_JSON_HERE}});
+
 });
--- _javascript/_slickquizjs/slickQuiz-config.js
+++ _javascript/_slickquizjs/slickQuiz-config.js
@@ -6,7 +6,8 @@ var quizJSON = {
     "info": {
         "name":    "Teste seus conhecimentos!!",
         "main":    "<p>Você se acha o 'bonzão' em cálculo numérico? Então teste seus conhecimentos com o nosso quiz.</p>",
-        "results": "<h5>Aprender mais:</h5><p>Acesse nosso material sobre cálculo numérico em <a href='video-aulas.html'>video-aulas</a> ou acesse o conteudo extra em nosso <a href='material-complementar.html'>material complementar</a>.</p>",
+        "results": "<h5>Aprender mais:</h5><p>Acesse nosso material sobre cálculo numérico em <a href='video-aulas.html'><span class='w3-tag w3-black w3-margin-bottom'>video-aulas</span></a> ou acesse o conteudo extra em nosso " +
+        "<a href='material-complementar.html'><span class='w3-tag w3-black w3-margin-bottom'>material complementar</span></a>.</p>",
         "level1":  "Sua mãe tem orgulho de você!",
         "level2":  "Aluno aplicado",
         "level3":  "Aluno esforçado",
@@ -73,7 +74,8 @@ var quizJSON2 = {
     "info": {
         "name":    "Teste seus conhecimentos!!",
         "main":    "<p>Você se acha o 'bonzão' em calculo númerico? Então teste seus conhecimentos com o nosso quiz.</p>",
-        "results": "<h5>Aprender mais:</h5><p>Acesse nosso material sobre cálculo numérico em <a href='video-aulas.html'>video-aulas</a> ou acesse o conteudo extra em nosso <a href='material-complementar.html'>material complementar</a>.</p>",
+        "results": "<h5>Aprender mais:</h5><p>Acesse nosso material sobre cálculo numérico em <a href='video-aulas.html'><span class='w3-tag w3-black w3-margin-bottom'>video-aulas</span></a> ou acesse o conteudo extra em nosso " +
+        "<a href='material-complementar.html'><span class='w3-tag w3-black w3-margin-bottom'>material complementar</span></a>.</p>",
         "level1":  "Sua mãe tem orgulho de você!",
         "level2":  "Aluno aplicado",
         "level3":  "Aluno esforçado",
@@ -124,6 +126,79 @@ var quizJSON2 = {
             ],
             "correct": "<p><span>Exelente!</span>Você acertou!</p>",
             "incorrect": "<p><span>Errou.</span>Resposta: Não, pois para todo valor $x_{k+1}$ que venha a ser calculado, é necessário fazer a verificação $f(x_{k-1}) * f(x_k) < 0$.</p>" // no comma here
+        },
+        { // Question 5
+            "q": "Dada a função $f(x) = x^2-1$, podemos afirmar que:<br>" +
+            "I) Os valores mais prováveis de $x_1$ e $x_2$ para o intervalo $[x_1, x_2]$ são $[0,1]$.<br><br>" +
+            "II) Dados os intervalos $[0,1]$ e $[-3,2]$, é melhor utilizar o maior intervalo, pois a quantidade de iterações diminuem.<br><br>" +
+            "III)Podem ser utilizados outros métodos, como o de Newton, para deixar o intervalo $[x_1, x_2]$  mais aproximado da raiz.<br><br>" +
+            "Está(ão) correta(s) a(s) alternativa(s):",
+            "a": [
+                {"option": "1 e 3",    "correct": true},
+                {"option": "1 e 2",     "correct": false}, // no comma here
+                {"option": "Apenas 1",     "correct": false}, // no comma here
+                {"option": "1, 2 e 3",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Bom trabalho!</span>Você acertou!</p>",
+            "incorrect": "<p><span>Errou :(</span>Resposta: 1 e 3.</p>" // no comma here
+        } // no comma here
+    ]
+};
+
+
+var quizJSON3 = {
+    "info": {
+        "name":    "Teste seus conhecimentos!!",
+        "main":    "<p>Você se acha o 'bonzão' em calculo númerico? Então teste seus conhecimentos com o nosso quiz.</p>",
+        "results": "<h5>Aprender mais:</h5><p>Acesse nosso material sobre cálculo numérico em <a href='video-aulas.html'><span class='w3-tag w3-black w3-margin-bottom'>video-aulas</span></a> ou acesse o conteudo extra em nosso " +
+        "<a href='material-complementar.html'><span class='w3-tag w3-black w3-margin-bottom'>material complementar</span></a>.</p>",
+        "level1":  "Sua mãe tem orgulho de você!",
+        "level2":  "Aluno aplicado",
+        "level3":  "Aluno esforçado",
+        "level4":  "Estude mais..",
+        "level5":  "Melhor você estudar um pouco mais..." // no comma here
+    },
+    "questions": [
+        { // Question 1 - Multiple Choice, Single True Answer
+            "q": "Sobre o método Regula Falsi, é correto afirmar:",
+            "a": [
+                {"option": "É o método quase igual ao método da secante, só difere pela questão de utilizar derivadas na determinação dos valores $x_{k+1}$.",      "correct": false},
+                {"option": "O método Regula Falsi é mais rápido que o método da Secante para encontrar a raiz; no entanto, tem menor desempenho do que o método do ponto fixo.",     "correct": false},
+                {"option": "É o único método que realiza a verificação de Bolzano em todas as iterações de $x_{k+1}$ para os valores  $x_{k-1}$ e $x_k$.",      "correct": true}
+            ],
+            "correct": "<p><span>Está certo!</span>Acertou.</p>",
+            "incorrect": "<p><span>Ah não, você errou :(</span>Resposta: É o único método que realiza a verificação de Bolzano em todas as iterações de x(k+1) para os valores  x(k-1) e xk.</p>" // no comma here
+        },
+        { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
+            "q": "Seja $f(x)$ uma função contínua num intervalo $[a, b]$. Se $f(a) f(b) < 0$ então existe pelo menos um ponto $x =Ɛ$ entre $a$ e $b$ que é zero de $f(x)$. Essa afirmação é:",
+            "a": [
+                {"option": "Falsa",               "correct": false},
+                {"option": "Verdadeira", "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Ótimo!</span>Você acertou.</p>",
+            "incorrect": "<p><span>Você errou.</span>Resposta: Verdadeira.</p>" // no comma here
+        },
+        { // Question 3 - Multiple Choice, Multiple True Answers, Select All
+            "q": "Qual dos métodos numérico abaixo não pertencem à classe dos métodos iterativos:",
+            "a": [
+                {"option": "Eliminação de Gauss",           "correct": true},
+                {"option": "Bissecção",                  "correct": false},
+                {"option": "Secante",  "correct": false},
+                {"option": "Newton",          "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Parabéns!</span>Você realmente está estudando em.. :D</p>",
+            "incorrect": "<p><span>Errou.</span>Resposta: Eliminação de Gauss.</p>" // no comma here
+        },
+        { // Question 4
+            "q": "Sobre a escolha do melhor método para calcular zero de funções, podemos afirmar:",
+            "a": [
+                {"option": "Método de Newton é o mais indicado, pois é fácil calcular $f'(x)$ e verificar as condições de convergência.",    "correct": false},
+                {"option": "A escolha do método está diretamente relacionado com a equação que se quer resolver, no que diz respeito ao comportamento da função na região da raiz exata, as dificuldades com o cálculo de $f'(x)$, critério de parada etc.",     "correct": true},
+                {"option": "Método da Secante é o mais indicado, pois é o método que converge mais rapidamente.",      "correct": false},
+                {"option": "Método da Bissecção é o mais indicado, pois é o método que efetua cálculos mais simples por interação.",   "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Exelente!</span>Você acertou!</p>",
+            "incorrect": "<p><span>Errou.</span>Resposta: A escolha do método está diretamente relacionado com a equação que se quer resolver, no que diz respeito ao comportamento da função na região da raiz exata, as dificuldades com o cálculo de f'(x), critério de parada etc.</p>" // no comma here
         },
         { // Question 5
             "q": "Dada a função $f(x) = x^2-1$, podemos afirmar que:<br>" +
